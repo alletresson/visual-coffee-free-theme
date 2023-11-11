9aab7d5694024659e2fab03d228f1854bfc1c2ec
Alle Sava <a@sava.io>
2023-11-11 18:14:57 +0100

    1.7

---
+++ footer.php
@@ -0,0 +1,11 @@
+</section> <!-- .grid -->
+</main>
+</div> <!-- #page -->
+
+<footer id="site-footer" itemscope itemtype="http://schema.org/WPFooter">
+	&copy;<?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
